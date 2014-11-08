a3e94ca39dd4b2cbc7af9df865b70a9f662486ba
KingJojo <funnygoman@gmail.com>
2014-11-08 05:42:13 -0800

    G fuckin g

--- pingpoint/app/src/main/java/com/pingpoint/pingpoint/FunctionActivity.java
+++ pingpoint/app/src/main/java/com/pingpoint/pingpoint/FunctionActivity.java
@@ -1,48 +1,20 @@
 package com.pingpoint.pingpoint;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
-import com.google.android.gms.maps.SupportMapFragment;
-
-import android.location.Location;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.app.FragmentTransaction;
 import android.app.ActionBar;
+import android.view.ViewGroup;
+import android.view.View;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.MapFragment;
 
-/**
- * This demo shows how GMS Location can be used to check for changes to the users location.  The
- * "My Location" button uses GMS Location to set the blue dot representing the users location. To
- * track changes to the users location on the map, we request updates from the
- * {@link com.google.android.gms.location.FusedLocationProviderApi}.
- */
-public class FunctionActivity extends FragmentActivity
-        implements
-        ConnectionCallbacks,
-        OnConnectionFailedListener,
-        LocationListener,
-        OnMyLocationButtonClickListener {
+public class FunctionActivity extends Activity {
 
-    private GoogleMap mMap;
-
-    private GoogleApiClient mGoogleApiClient;
-    private TextView mMessageView;
-
-    // These settings are the same as the settings for the map. They will in fact give you updates
-    // at the maximal rates currently possible.
-    private static final LocationRequest REQUEST = LocationRequest.create()
-            .setInterval(5000)         // 5 seconds
-            .setFastestInterval(16)    // 16ms = 60fps
-            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    private GoogleMap theMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,101 +25,36 @@ public class FunctionActivity extends FragmentActivity
         decorView.setSystemUiVisibility(uiOptions);
         ActionBar actionBar = getActionBar();
         actionBar.hide();
-        //mMessageView = (TextView) findViewById(R.id.message_text);
+        theMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        MapFragment mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map, mMapFragment);
+        fragmentTransaction.commit();
+        theMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //setUpMapIfNeeded();
-        setUpGoogleApiClientIfNeeded();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    private void setUpMapIfNeeded() {
+    private void setUpMapIfNeeded()
+    {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+        if (theMap == null) {
+            theMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                mMap.setMyLocationEnabled(true);
-                mMap.setOnMyLocationButtonClickListener(this);
+            if (theMap != null) {
+                // The Map is verified. It is now safe to manipulate the map.
+
             }
         }
     }
 
-    private void setUpGoogleApiClientIfNeeded() {
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-        }
-    }
-
-    /**
-     * Button to get current Location. This demonstrates how to get the current Location as required
-     * without needing to register a LocationListener.
+    /* updateLocation()
+     * - Get Own User Location
+     * - Upload said Location to server
+     * - Bring back list of EVERY thing in server (AKA GROUP)
+     * - Display other locations as PINGS
      */
-    public void showMyLocation(View view) {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            String msg = "Location = "
-                    + LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    /**
-     * Implementation of {@link LocationListener}.
-     */
-    @Override
-    public void onLocationChanged(Location location) {
-        mMessageView.setText("Location = " + location);
-    }
-
-    /**
-     * Callback called when connected to GCore. Implementation of {@link ConnectionCallbacks}.
-     */
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient,
-                REQUEST,
-                this);  // LocationListener
-    }
-
-    /**
-     * Callback called when disconnected from GCore. Implementation of {@link ConnectionCallbacks}.
-     */
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // Do nothing
-    }
-
-    /**
-     * Implementation of {@link OnConnectionFailedListener}.
-     */
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Do nothing
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false;
-    }
+    /* */
 }
