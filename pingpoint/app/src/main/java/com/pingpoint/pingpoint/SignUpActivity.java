579dea1ce07b4a66fabd9b136cba686855d3af9c
nng555 <n.ng555@gmail.com>
2014-11-08 00:12:59 -0800

    Merge branch 'master' of https://github.com/nng555/pingpoint
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- pingpoint/app/src/main/java/com/pingpoint/pingpoint/SignUpActivity.java
+++ pingpoint/app/src/main/java/com/pingpoint/pingpoint/SignUpActivity.java
@@ -62,41 +62,6 @@ public class SignUpActivity extends Activity {
     String password = passwordEditText.getText().toString().trim();
     String passwordAgain = passwordAgainEditText.getText().toString().trim();
 
-    // Validate the sign up data
-    boolean validationError = false;
-    StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
-    if (username.length() == 0) {
-      validationError = true;
-      validationErrorMessage.append(getString(R.string.error_blank_username));
-    }
-    if (password.length() == 0) {
-      if (validationError) {
-        validationErrorMessage.append(getString(R.string.error_join));
-      }
-      validationError = true;
-      validationErrorMessage.append(getString(R.string.error_blank_password));
-    }
-    if (!password.equals(passwordAgain)) {
-      if (validationError) {
-        validationErrorMessage.append(getString(R.string.error_join));
-      }
-      validationError = true;
-      validationErrorMessage.append(getString(R.string.error_mismatched_passwords));
-    }
-    validationErrorMessage.append(getString(R.string.error_end));
-
-    // If there is a validation error, display the error
-    if (validationError) {
-      Toast.makeText(SignUpActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
-          .show();
-      return;
-    }
-
-    // Set up a progress dialog
-    final ProgressDialog dialog = new ProgressDialog(SignUpActivity.this);
-    dialog.setMessage(getString(R.string.progress_signup));
-    dialog.show();
-
     // Set up a new Parse user
     ParseUser user = new ParseUser();
     user.setUsername(username);
@@ -106,7 +71,6 @@ public class SignUpActivity extends Activity {
     user.signUpInBackground(new SignUpCallback() {
       @Override
       public void done(ParseException e) {
-        dialog.dismiss();
         if (e != null) {
           // Show the error message
           Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
