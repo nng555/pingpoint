b319c1ea54e0086948dc924c018711532bab8e21
nng555 <n.ng555@gmail.com>
2014-11-07 22:19:16 -0800

    fukkkk

--- app/src/main/java/com/pingpoint/pingpoint/WelcomeActivity.java
+++ app/src/main/java/com/pingpoint/pingpoint/WelcomeActivity.java
@@ -1,36 +1,38 @@
-package com.pingpoint.pingpoint;
+package com.parse.anywall;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-
+/**
+ * Activity which displays a registration screen to the user.
+ */
 public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-    }
 
+        // Log in button click handler
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Starts an intent of the log in activity
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.welcome, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        // Sign up button click handler
+        Button signupButton = (Button) findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Starts an intent for the sign up activity
+                startActivity(new Intent(WelcomeActivity.this, SignUpActivity.class));
+            }
+        });
     }
 }
